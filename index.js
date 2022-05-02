c426f6335fa002742ad3a81b7ff8e9689a1db434
Your Name <you@example.com>
2022-05-02 21:46:54 +0600

    heroku file added and data loaded

--- index.js
+++ index.js
@@ -8,6 +8,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {res.send('Windsor Car Warehouse is Running')});
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = 
+`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dh4fn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+  try{
+     await client.connect();
+     const itemsCollection = client.db('WindsorCar').collection('items');
+     app.get('/', (req, res) => {res.send('Windsor Car Warehouse is Running')});
+
+    //  load data 
+    app.get('/items', async(req, res) => {
+      const query = {};
+      const cursor = itemsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    });
+
+    // single items
+    app.get('/items/:id' , async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await itemsCollection.findOne(query);
+      res.send(result);
+    })
+
+  }
+  finally{
+    // await client.close();
+  }
+}
+run().catch(console.dir)
+
+
+
 
 app.listen(port, () => {console.log('This server is listening', port)})
